fce29c7b8962add65d7d1ce01a81562378c2dc29
Elodie Gateau <elodie.gateau.raffeneau@gmail.com>
2026-04-12 17:31:00 +0200

    ✨ Create footer

--- assets/components/layout/AppFooter.vue
+++ assets/components/layout/AppFooter.vue
@@ -1,8 +1,19 @@
 <script setup>
+import { Icon } from '@iconify/vue'
+import { useI18n } from "vue-i18n";
 
+const { t, locale } = useI18n();
 </script>
 
 <template>
+
+    <Icon icon="arcticons:emoji-copyright-sign" />
+    <span>2026</span>
+    <span> - {{ t('main.title') }}</span>
+    <span> - {{ t('footer.mentions')}}</span>
+    <span> - {{ t('footer.made_part1')}}</span>
+    <Icon icon="fa:music" />
+    <span>{{ t('footer.made_part2')}}</span>
 
 </template>
 
